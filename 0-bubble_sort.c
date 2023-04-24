ae302b1bcc4d4fd7323c7ad6675928a95f719351
Delegat-is <kelvinjuma903@gmail.com>
2023-04-24 16:11:15 -0700

    Bubble Sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,25 +10,28 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j;
+	size_t i, j;
 	int temp;
+	bool swapped;
 
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = false;
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
-				array[j + 1] = temp;
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				swapped = true;
 				print_array(array, size);
 			}
 		}
+		if (!swapped)
+			break;
 	}
-
 }
