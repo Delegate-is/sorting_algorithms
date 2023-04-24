9c4e64657ce85cdd9cda2cd106f48849eb6ec0fc
Delegat-is <kelvinjuma903@gmail.com>
2023-04-24 16:39:31 -0700

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorting an integer array with Selection sort algorithm
+ * Args:
+ *	@array: array being sorted
+ *	@size: size of array being sorted
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+	int tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		if (min_idx != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			printf("Swap: %d <-> %d\n", array[i], array[min_idx]);
+			print_array(array, size);
+		}
+	}
+}
