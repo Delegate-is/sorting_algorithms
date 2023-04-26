0afd14040076337b056531707847a99c8c5f80b8
Delegat-is <kelvinjuma903@gmail.com>
2023-04-26 04:46:42 -0700

    Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,9 +8,9 @@
  */
 void swap_ints(int *a, int *b)
 {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -25,30 +25,31 @@ void swap_ints(int *a, int *b)
  */
 int partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[high];
-    int i = low - 1;
+	int pivot = array[high];
+	int i = low - 1;
 
-    int j;
-    for (j = low; j < high; j++)
-    {
-        if (array[j] < pivot)
-        {
-            i++;
-            if (i != j)
-            {
-                swap_ints(&array[i], &array[j]);
-                print_array(array, size);
-            }
-        }
-    }
+	int j;
 
-    if (i + 1 != high)
-    {
-        swap_ints(&array[i + 1], &array[high]);
-        print_array(array, size);
-    }
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap_ints(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
 
-    return (i + 1);
+	if (i + 1 != high)
+	{
+		swap_ints(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+
+	return (i + 1);
 }
 
 /**
@@ -61,12 +62,13 @@ int partition(int *array, int low, int high, size_t size)
  */
 void quicksort(int *array, int low, int high, size_t size)
 {
-    if (low < high)
-    {
-        int pivot = partition(array, low, high, size);
-        quicksort(array, low, pivot - 1, size);
-        quicksort(array, pivot + 1, high, size);
-    }
+	if (low < high)
+	{
+		int pivot = partition(array, low, high, size);
+
+		quicksort(array, low, pivot - 1, size);
+		quicksort(array, pivot + 1, high, size);
+	}
 }
 
 /**
@@ -77,7 +79,7 @@ void quicksort(int *array, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array && size > 1)
-        quicksort(array, 0, size - 1, size);
+	if (array && size > 1)
+		quicksort(array, 0, size - 1, size);
 }
 
